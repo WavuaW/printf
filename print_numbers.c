6b46d66c7a6364025551cfa8a7c8977c7d33cc48
Dennis <dmutuimatthews@gmail.com>
2022-08-13 08:34:29 +0000

    More changes

--- print_numbers.c
+++ print_numbers.c
@@ -5,6 +5,7 @@
  * @args: List of arguments
  * Return: The number of arguments printed
  */
+
 int print_number(va_list args)
 {
 	int n;
@@ -33,7 +34,6 @@ int print_number(va_list args)
 		num %= div;
 		div /= 10;
 	}
-
 	return (len);
 }
 /**
@@ -41,6 +41,7 @@ int print_number(va_list args)
  * @n: unsigned integer to be printed
  * Return: The amount of numbers printed
  */
+
 int print_unsgined_number(unsigned int n)
 {
 	int div;
@@ -54,13 +55,11 @@ int print_unsgined_number(unsigned int n)
 
 	for (; num / div > 9; )
 		div *= 10;
-
 	for (; div != 0; )
 	{
 		len += _write_char('0' + num / div);
 		num %= div;
 		div /= 10;
 	}
-
 	return (len);
 }
